d0db0a1eb21a445718a062b5e52fda3363980726
f2dev898 <fernando.git898@gmail.com>
2022-10-28 22:04:28 +0000

    Atualização da tela de login e cadastro

--- src/pages/cadastro/Index.tsx
+++ src/pages/cadastro/Index.tsx
@@ -1,6 +1,4 @@
-import { useNavigate } from "react-router-dom"
-import Logo from "../../assets/logo1.png"
-import { Container } from "@mui/material"
+import { useNavigate, Link } from "react-router-dom"
 import { useState } from "react"
 
 export default function CadastroUsuario() {
@@ -23,21 +21,23 @@ export default function CadastroUsuario() {
 
     function onSubmit(event: any) {
         event.preventDefault();
+
+        Navigate('/panel')
     }
 
     return (
 
-        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', minHeight: '100vh', backgroundColor: '#06283D', color: 'white' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#06283D', color: 'white' }}>
 
             <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', width: '100%' }}>
 
 
                 <div className="container py-5 col-md-7 col-lg-8">
-                    <h3 className="pb-2 border-bottom">Solicitar Pedido</h3>
-                    <form className="needs-validation" >
+                    <h3 className="pb-2 border-bottom">Cadastro de Cliente</h3>
+                    <form className="needs-validation" onSubmit={onSubmit}>
                         <div className="row g-3">
                             <div className="col-sm-6">
-                                <label htmlFor="firstName" className="form-label">Nome Produto</label>
+                                <label htmlFor="firstName" className="form-label">Nome</label>
                                 <input type="text" className="form-control" id="firstName" placeholder="" />
                                 <div className="invalid-feedback">
                                     Valid first name is required.
@@ -45,23 +45,17 @@ export default function CadastroUsuario() {
                             </div>
 
                             <div className="col-sm-6">
-                                <label htmlFor="lastName" className="form-label">Status</label>
-                                <input type="text" className="form-control" id="lastName" placeholder="" value={"Aguardando Recebimento"} disabled readOnly />
+                                <label htmlFor="lastName" className="form-label">Sobrenome</label>
+                                <input type="text" className="form-control" id="lastName" placeholder=""/>
                                 <div className="invalid-feedback">
                                     Valid last name is required.
                                 </div>
                             </div>
 
                             <div className="col-12">
-                                <label htmlFor="exampleFormControlTextarea1" className="form-label">Descrição Produto</label>
-                                <textarea className="form-control" id="exampleFormControlTextarea1" rows={2}></textarea>
-                            </div>
-
-                            <div className="col-12">
-                                <label htmlFor="username" className="form-label">Código do Pedido</label>
+                                <label htmlFor="username" className="form-label">CPF</label>
                                 <div className="input-group has-validation">
-                                    <span className="input-group-text">@</span>
-                                    <input type="text" className="form-control" id="username" placeholder="P00-000000000" disabled readOnly />
+                                    <input type="text" className="form-control" id="username" placeholder="000.000.000-00"  />
                                     <div className="invalid-feedback">
                                         Your username is required.
                                     </div>
@@ -70,19 +64,14 @@ export default function CadastroUsuario() {
 
                             <div className="col-12">
                                 <label htmlFor="address" className="form-label">Endereço</label>
-                                <select className="form-select" aria-label="Default select example">
-                                    <option selected>Open this select menu</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
+                                <input type="text" className="form-control" id="firstName" placeholder="" />
                                 <div className="invalid-feedback">
                                     Please enter your shipping address.
                                 </div>
                             </div>
 
                             <div className="col-12">
-                                <label htmlFor="email" className="form-label">Email <span className="text-muted">(Optional)</span></label>
+                                <label htmlFor="email" className="form-label">Email</label>
                                 <input type="email" className="form-control" id="email" placeholder="you@example.com" />
                                 <div className="invalid-feedback">
                                     Please enter a valid email address htmlFor shipping updates.
